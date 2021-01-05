4c81f46f1fb35dbf60525b5a93653d41597e59ca
Emma Buller <dib5017@gmail.com>
2021-01-05 13:21:57 -0500

    Added get and set methods for Node

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+  private String data;
+  private Node next,prev;
+
+  public void setData(String d) {
+    data = d;
+  }
+
+  public String getData() {
+    return data;
+  }
+}
---
+++ nodeTester.java
@@ -0,0 +1,7 @@
+public class nodeTester {
+  public static void main(String[] args) {
+    Node n = new Node();
+    n.setData("hi");
+    System.out.println(n.getData());
+  }
+}
