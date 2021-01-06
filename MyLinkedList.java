13ec0a5d9bb2606a93b17400a4617be1a6ba2786
Emma Buller <dib5017@gmail.com>
2021-01-06 16:38:19 -0500

    Tested constructor and size for linked list

---
+++ MyLinkedList.java
@@ -0,0 +1,27 @@
+public class MyLinkedList{
+ private int size;
+ private Node start,end;
+ public MyLinkedList() {
+   size = 0;
+   start = null;
+   end = null;
+ }
+ public int size() {
+   return size;
+ }
+ public boolean add(String value) {
+   return true;
+ }
+ public void add(int index, String value) {
+   System.out.println("what");
+ }
+ public String get(int index) {
+   return null;
+ }
+ public String set(int index, String value) {
+   return null;
+ }
+ public String toString() {
+   return null;
+ }
+}
---
+++ listTester.java
@@ -0,0 +1,6 @@
+public class listTester {
+  public static void main(String[] args) {
+    MyLinkedList m = new MyLinkedList();
+    System.out.println(m.size());
+  }
+}
