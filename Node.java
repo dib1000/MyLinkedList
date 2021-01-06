5e73fae51436ff4c109915a577806545e010e81c
Emma Buller <dib5017@gmail.com>
2021-01-06 15:59:45 -0500

    Made constructor

--- Node.java
+++ Node.java
@@ -2,6 +2,12 @@ public class Node {
   private String data;
   private Node next,prev;
 
+  public Node(String value) {
+    data = value;
+    next = null;
+    prev = null;
+  }
+
   public void setData(String d) {
     data = d;
   }
--- nodeTester.java
+++ nodeTester.java
@@ -1,6 +1,7 @@
 public class nodeTester {
   public static void main(String[] args) {
-    Node n = new Node();
+    Node n = new Node("bye");
+    System.out.println(n.getData());
     n.setData("hi");
     System.out.println(n.getData());
   }
